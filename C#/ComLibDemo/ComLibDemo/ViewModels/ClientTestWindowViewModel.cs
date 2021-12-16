b1427617bb71846bd9dd3cd207144da8b88d27c2
tsushoji <s.tuji3007sho@gmail.com>
2021-12-17 02:48:39 +0900

    デモアプリ画面追加_ver1.0.0 #1

--- C#/ComLibDemo/ComLibDemo/ViewModels/ClientTestWindowViewModel.cs
+++ C#/ComLibDemo/ComLibDemo/ViewModels/ClientTestWindowViewModel.cs
@@ -1,10 +1,89 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 
 namespace ComLibDemo.ViewModels
 {
     public class ClientTestWindowViewModel : BindableBase
     {
+        public string OutputTextBoxText { get; set; } = string.Empty;
+
+        public string InputSendIPTextBoxText { get; set; } = string.Empty;
+
+        public string InputSendPortTextBoxText { get; set; } = string.Empty;
+
+        public string InputSendStringTextBoxText { get; set; } = string.Empty;
+
+        private bool _isEnabledClientConnectButton = true;
+        public bool IsEnabledClientConnectButton
+        {
+            get
+            {
+                return _isEnabledClientConnectButton;
+            }
+
+            set
+            {
+                SetProperty(ref _isEnabledClientConnectButton, value);
+            }
+        }
+
+        private bool _isEnabledClientDisconnectButton = true;
+        public bool IsEnabledClientDisconnectButton
+        {
+            get
+            {
+                return _isEnabledClientDisconnectButton;
+            }
+
+            set
+            {
+                SetProperty(ref _isEnabledClientDisconnectButton, value);
+            }
+        }
+
+        private bool _isEnabledClientSendDataButton = true;
+        public bool IsEnabledClientSendDataButton
+        {
+            get
+            {
+                return _isEnabledClientSendDataButton;
+            }
+
+            set
+            {
+                SetProperty(ref _isEnabledClientSendDataButton, value);
+            }
+        }
+
+        public DelegateCommand ClientConnectClicked { get; private set; }
+
+        public DelegateCommand ClientDisconnectClicked { get; private set; }
+
+        public DelegateCommand ClientSendDataClicked { get; private set; }
+
         public ClientTestWindowViewModel()
+        {
+            SetEvent();
+        }
+
+        private void SetEvent()
+        {
+            ClientConnectClicked = new DelegateCommand(OnConnect);
+            ClientDisconnectClicked = new DelegateCommand(OnDisconnect);
+            ClientSendDataClicked = new DelegateCommand(OnSendData);
+        }
+
+        private void OnConnect()
+        {
+
+        }
+
+        private void OnDisconnect()
+        {
+
+        }
+
+        private void OnSendData()
         {
 
         }
--- C#/ComLibDemo/ComLibDemo/ViewModels/ServerTestWindowViewModel.cs
+++ C#/ComLibDemo/ComLibDemo/ViewModels/ServerTestWindowViewModel.cs
@@ -1,10 +1,63 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 
 namespace ComLibDemo.ViewModels
 {
     public class ServerTestWindowViewModel : BindableBase
     {
+        public string OutputTextBoxText { get; set; } = string.Empty;
+
+        public string InputListenPortTextBoxText { get; set; } = string.Empty;
+
+        private bool _isEnabledServerStartServiceButton = true;
+        public bool IsEnabledServerStartServiceButton
+        {
+            get
+            {
+                return _isEnabledServerStartServiceButton;
+            }
+
+            set
+            {
+                SetProperty(ref _isEnabledServerStartServiceButton, value);
+            }
+        }
+
+        private bool _isEnabledServerEndServiceButton = true;
+        public bool IsEnabledServerEndServiceButton
+        {
+            get
+            {
+                return _isEnabledServerEndServiceButton;
+            }
+
+            set
+            {
+                SetProperty(ref _isEnabledServerEndServiceButton, value);
+            }
+        }
+
+        public DelegateCommand ServerStartServiceClicked { get; private set; }
+
+        public DelegateCommand ServerEndServiceClicked { get; private set; }
+
         public ServerTestWindowViewModel()
+        {
+            SetEvent();
+        }
+
+        private void SetEvent()
+        {
+            ServerStartServiceClicked = new DelegateCommand(OnStartService);
+            ServerEndServiceClicked = new DelegateCommand(OnEndService);
+        }
+
+        private void OnStartService()
+        {
+
+        }
+
+        private void OnEndService()
         {
 
         }
